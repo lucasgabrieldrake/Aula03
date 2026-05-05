dfe622013f9b7ebdd4e5b18b327802afd5ab1cfc
lucasgabrieldrake <gabriellucasribeiro18@gmail.com>
2026-05-04 21:42:20 -0300

    Implementa arquivo atividade 01

--- aula03/atividade01.py
+++ aula03/atividade01.py
@@ -1,1 +1,11 @@
+#Pedir um número da compra, aplicar desconto, exibir valor com o desconto 16%, caso contrario exibir valor original sem desconto(caso não ultrapasse 250)
 
+valor = float(input('Informe o valor: '))
+
+if valor > 250:
+   desconto = valor * 0.16
+   total = valor - desconto
+   print(f'Valor com desconto de 16% R${total}')
+
+else:
+   print(f'Valor sem desconto: R$ {valor}')
